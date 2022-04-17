8625b380cccc7c75026bf7415fe41cf4f00624ce
Antesser <kairon-ds@mail.ru>
2022-04-17 17:35:30 +0300

    1

--- cpu-ram usage monitor bar.py
+++ cpu-ram usage monitor bar.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import ttk
+import sys
 
 class Appication(tk.Tk):
 
@@ -8,6 +10,18 @@ class Appication(tk.Tk):
         self.attributes("-topmost", True)
         self.overrideredirect(True)
         self.resizable(False, False)
+        self.title("CPU_RAM usage monitor bar") 
+
+        self.set_ui()
+
+    def set_ui(self):
+        exit_button = ttk.Button(self, text = "Exit", command = self.app_exit)
+        exit_button.pack(fill=tk.X) #pack() упаковщик, который собирает кнопку и размещает на окне, fill=tk.X - растянуть по горизонтали
+
+    def app_exit(self):
+        self.destroy() #разрушить окно
+        sys.exit() #убить процесс
+
 
 root = Appication()
 root.mainloop()
